fd31466dac6f55722f7da68d0f43f7cb71dac62a
Andrew Borys <borysandrey1988@gmail.com>
2024-10-11 09:37:10 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Button from '@mui/material/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+    </div>
+  );
+}
+
+export default App;
